c4744639ecf83f198c8fe9903aef1204068d75cd
MorganEJLA <mojones311@gmail.com>
2025-01-19 23:54:49 -0500

    first commit

---
+++ src/components/UserInput.jsx
@@ -0,0 +1,16 @@
+export default function UserInput() {
+  return (
+    <section id="user-id">
+      <div className="input-group">
+        <p>
+          <label></label>
+          <input />
+        </p>
+        <p>
+          <label></label>
+          <input />
+        </p>
+      </div>
+    </section>
+  );
+}
